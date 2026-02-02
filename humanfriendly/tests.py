a02aef737c020f814f08a7df16c9fe0cf17a3331
Shelvacu <git@shelvacu.com>
2026-02-02 15:56:27 -0800

    fix test_ansi_style

--- humanfriendly/tests.py
+++ humanfriendly/tests.py
@@ -1008,7 +1008,7 @@ class HumanFriendlyTestCase(TestCase):
         assert ansi_style(background='blue') == '%s44%s' % (ANSI_CSI, ANSI_SGR)
         assert ansi_style(color='blue', bright=True) == '%s94%s' % (ANSI_CSI, ANSI_SGR)
         assert ansi_style(color=214) == '%s38;5;214%s' % (ANSI_CSI, ANSI_SGR)
-        assert ansi_style(background=214) == '%s39;5;214%s' % (ANSI_CSI, ANSI_SGR)
+        assert ansi_style(background=214) == '%s48;5;214%s' % (ANSI_CSI, ANSI_SGR)
         assert ansi_style(color=(0, 0, 0)) == '%s38;2;0;0;0%s' % (ANSI_CSI, ANSI_SGR)
         assert ansi_style(color=(255, 255, 255)) == '%s38;2;255;255;255%s' % (ANSI_CSI, ANSI_SGR)
         assert ansi_style(background=(50, 100, 150)) == '%s48;2;50;100;150%s' % (ANSI_CSI, ANSI_SGR)
